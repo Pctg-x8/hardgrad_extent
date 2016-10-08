c2dcd90f1df956d0fed67153b85a8e36dac66bdd
S.Percentage <Syn.Tri.Naga@gmail.com>
2016-10-08 20:36:14 +0900

    added parsetools

---
+++ src/postludium/parsetools.rs
@@ -0,0 +1,29 @@
+// ParseTools for reference to slice of char
+
+pub trait ParseTools
+{
+	type Item;
+	fn skip_while<F>(self, pred: F) -> Self where F: Fn(Self::Item) -> bool;
+	fn take_while<F>(self, pred: F) -> (Self, Self) where F: Fn(Self::Item) -> bool;
+	fn drop(self, count: usize) -> Self;
+	fn clone_as_string(self) -> String;
+}
+impl<'a> ParseTools for &'a [char]
+{
+	type Item = char;
+	fn skip_while<F>(self, pred: F) -> Self where F: Fn(char) -> bool
+	{
+		if !self.is_empty() && pred(self[0]) { Self::skip_while(&self[1..], pred) } else { self }
+	}
+	fn take_while<F>(self, pred: F) -> (Self, Self) where F: Fn(char) -> bool
+	{
+		fn _impl<F>(input: &[char], counter: usize, pred: F) -> usize where F: Fn(char) -> bool
+		{
+			if !input.is_empty() && pred(input[0]) { _impl(&input[1..], counter + 1, pred) } else { counter }
+		}
+		let len = _impl(self, 0, pred);
+		(&self[..len], &self[len..])
+	}
+	fn drop(self, count: usize) -> Self { &self[count..] }
+	fn clone_as_string(self) -> String { self.into_iter().cloned().collect() }
+}
