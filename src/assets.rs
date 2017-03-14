59c71bb4ef5eb462bb3d102c891314aea759eaee
S.Percentage <Syn.Tri.Naga@gmail.com>
2017-03-14 18:55:34 +0900

    followed interlude changes

--- src/assets.rs
+++ src/assets.rs
@@ -5,29 +5,31 @@ use interlude::*;
 use interlude::ffi::*;
 use std::mem::size_of;
 use structures::*;
+use constants::*;
 use logical_resources::*;
 use std::ops::Deref;
+use std::rc::Rc;
 
 pub struct ShaderStore
 {
 	// Vertex Shaders //
-	pub geometry_preinstancing_vsh: ShaderProgram,
-	pub erz_preinstancing_vsh: ShaderProgram,
-	pub player_rotate_vsh: ShaderProgram,
-	pub playerbullet_vsh: ShaderProgram,
-	pub lineburst_particle_vsh: ShaderProgram,
-	pub gridrender_vsh: ShaderProgram,
-	pub bullet_vsh: ShaderProgram,
+	pub geometry_preinstancing_vsh: Rc<VertexShader>,
+	pub erz_preinstancing_vsh: Rc<VertexShader>,
+	pub player_rotate_vsh: Rc<VertexShader>,
+	pub playerbullet_vsh: Rc<VertexShader>,
+	pub lineburst_particle_vsh: Rc<VertexShader>,
+	pub gridrender_vsh: Rc<VertexShader>,
+	pub bullet_vsh: Rc<VertexShader>,
 	// Geometry Shaders //
-	pub enemy_duplication_gsh: ShaderProgram,
-	pub background_duplication_gsh: ShaderProgram,
-	pub enemy_rezonator_duplication_gsh: ShaderProgram,
-	pub lineburst_particle_instantiate_gsh: ShaderProgram,
+	pub enemy_duplication_gsh: Rc<GeometryShader>,
+	pub background_duplication_gsh: Rc<GeometryShader>,
+	pub enemy_rezonator_duplication_gsh: Rc<GeometryShader>,
+	pub lineburst_particle_instantiate_gsh: Rc<GeometryShader>,
 	// Fragment Shaders //
-	pub solid_fsh: ShaderProgram,
-	pub sprite_fsh: ShaderProgram,
-	pub tonemap_fsh: ShaderProgram,
-	pub colored_sprite_fsh: ShaderProgram
+	pub solid_fsh: Rc<FragmentShader>,
+	pub sprite_fsh: Rc<FragmentShader>,
+	pub tonemap_fsh: Rc<FragmentShader>,
+	pub colored_sprite_fsh: Rc<FragmentShader>
 }
 impl ShaderStore
 {
@@ -35,57 +37,57 @@ impl ShaderStore
 	{
 		ShaderStore
 		{
-			geometry_preinstancing_vsh: ShaderProgram::new_vertex_from_asset(engine, "shaders.GeometryPreinstancing", "main", &[
+			geometry_preinstancing_vsh: VertexShader::from_asset(engine, "shaders.GeometryPreinstancing", "main", &[
 				VertexBinding::PerVertex(size_of::<CVector4>() as u32),
 				VertexBinding::PerInstance(size_of::<u32>() as u32)
 			], &[
 				VertexAttribute(0, VkFormat::R32G32B32A32_SFLOAT, 0),
 				VertexAttribute(1, VkFormat::R32_UINT, 0)
 			]).or_crash(),
-			erz_preinstancing_vsh: ShaderProgram::new_vertex_from_asset(engine, "shaders.EnemyRezonatorV", "main", &[
+			erz_preinstancing_vsh: VertexShader::from_asset(engine, "shaders.EnemyRezonatorV", "main", &[
 				VertexBinding::PerVertex(size_of::<CVector4>() as u32),
 				VertexBinding::PerInstance(size_of::<CVector4>() as u32)
 			], &[
 				VertexAttribute(0, VkFormat::R32G32B32A32_SFLOAT, 0),
 				VertexAttribute(1, VkFormat::R32G32B32A32_SFLOAT, 0)
 			]).or_crash(),
-			player_rotate_vsh: ShaderProgram::new_vertex_from_asset(engine, "shaders.PlayerRotor", "main", &[
+			player_rotate_vsh: VertexShader::from_asset(engine, "shaders.PlayerRotor", "main", &[
 				VertexBinding::PerVertex(size_of::<CVector4>() as u32),
 				VertexBinding::PerInstance(size_of::<CVector4>() as u32)
 			], &[
 				VertexAttribute(0, VkFormat::R32G32B32A32_SFLOAT, 0),
 				VertexAttribute(1, VkFormat::R32G32B32A32_SFLOAT, 0)
 			]).or_crash(),
-			playerbullet_vsh: ShaderProgram::new_vertex_from_asset(engine, "shaders.PlayerBullet", "main", &[
+			playerbullet_vsh: VertexShader::from_asset(engine, "shaders.PlayerBullet", "main", &[
 				VertexBinding::PerVertex(size_of::<CVector4>() as u32),
 				VertexBinding::PerInstance(size_of::<CVector4>() as u32)
 			], &[
 				VertexAttribute(0, VkFormat::R32G32B32A32_SFLOAT, 0),
 				VertexAttribute(1, VkFormat::R32G32B32A32_SFLOAT, 0)
 			]).or_crash(),
-			lineburst_particle_vsh: ShaderProgram::new_vertex_from_asset(engine, "shaders.LineBurstParticleVert", "main", &[
+			lineburst_particle_vsh: VertexShader::from_asset(engine, "shaders.LineBurstParticleVert", "main", &[
 				VertexBinding::PerVertex(size_of::<LineBurstParticleGroup>() as u32)
 			], &[
 				VertexAttribute(0, VkFormat::R32_UINT, 0),
 				VertexAttribute(0, VkFormat::R32G32_SFLOAT, size_of::<u32>() as u32)
 			]).or_crash(),
-			bullet_vsh: ShaderProgram::new_vertex_from_asset(engine, "shaders.BulletVert", "main", &[
+			bullet_vsh: VertexShader::from_asset(engine, "shaders.BulletVert", "main", &[
 				VertexBinding::PerVertex(size_of::<CVector4>() as u32),
 				VertexBinding::PerInstance(size_of::<CVector2>() as u32)
 			], &[
 				VertexAttribute(0, VkFormat::R32G32B32A32_SFLOAT, 0),
 				VertexAttribute(1, VkFormat::R32G32_SFLOAT, 0)
 			]).or_crash(),
-			gridrender_vsh: ShaderProgram::new_vertex_from_asset(engine, "shaders.GridRenderV", "main",
+			gridrender_vsh: VertexShader::from_asset(engine, "shaders.GridRenderV", "main",
 				&[VertexBinding::PerVertex(size_of::<Position>() as u32)], &[VertexAttribute(0, VkFormat::R32G32B32A32_SFLOAT, 0)]).or_crash(),
-			enemy_duplication_gsh: ShaderProgram::new_geometry_from_asset(engine, "shaders.EnemyDuplicator", "main").or_crash(),
-			background_duplication_gsh: ShaderProgram::new_geometry_from_asset(engine, "shaders.BackLineDuplicator", "main").or_crash(),
-			enemy_rezonator_duplication_gsh: ShaderProgram::new_geometry_from_asset(engine, "shaders.EnemyRezonatorDup", "main").or_crash(),
-			lineburst_particle_instantiate_gsh: ShaderProgram::new_geometry_from_asset(engine, "shaders.LineBurstParticleInstantiate", "main").or_crash(),
-			solid_fsh: ShaderProgram::new_fragment_from_asset(engine, "shaders.ThroughColor", "main").or_crash(),
-			sprite_fsh: ShaderProgram::new_fragment_from_asset(engine, "shaders.SpriteFrag", "main").or_crash(),
-			tonemap_fsh: ShaderProgram::new_fragment_from_asset(engine, "shaders.SaturatingToneMap", "main").or_crash(),
-			colored_sprite_fsh: ShaderProgram::new_fragment_from_asset(engine, "shaders.ColoredSpriteFrag", "main").or_crash()
+			enemy_duplication_gsh: GeometryShader::from_asset(engine, "shaders.EnemyDuplicator", "main").or_crash(),
+			background_duplication_gsh: GeometryShader::from_asset(engine, "shaders.BackLineDuplicator", "main").or_crash(),
+			enemy_rezonator_duplication_gsh: GeometryShader::from_asset(engine, "shaders.EnemyRezonatorDup", "main").or_crash(),
+			lineburst_particle_instantiate_gsh: GeometryShader::from_asset(engine, "shaders.LineBurstParticleInstantiate", "main").or_crash(),
+			solid_fsh: FragmentShader::from_asset(engine, "shaders.ThroughColor", "main").or_crash(),
+			sprite_fsh: FragmentShader::from_asset(engine, "shaders.SpriteFrag", "main").or_crash(),
+			tonemap_fsh: FragmentShader::from_asset(engine, "shaders.SaturatingToneMap", "main").or_crash(),
+			colored_sprite_fsh: FragmentShader::from_asset(engine, "shaders.ColoredSpriteFrag", "main").or_crash()
 		}
 	}
 }
@@ -106,7 +108,8 @@ impl ApplicationBufferData
 			BufferContent::Vertex(size_of::<InstanceMemory>()),
 			BufferContent::Vertex(size_of::<[PosUV; 4]>()),
 			BufferContent::Vertex(size_of::<VertexMemoryForWireRender>()),
-			BufferContent::Index(size_of::<IndexMemory>())
+			BufferContent::Index(size_of::<IndexMemory>()),
+			BufferContent::Custom(VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, size_of::<[PosUV; 4]>() * MAX_BPARTICLES)
 		]);
 		let (application_data, appdata_stage) = application_buffer_prealloc.instantiate().or_crash();
 		let this = ApplicationBufferData { alloc_bp: application_buffer_prealloc.independence(), dev: application_data, stg: appdata_stage };
